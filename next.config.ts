71ae02eab07b7b96f3394f6d2866f3410abaaebd
sasanktumpati <sasanktumpati16@gmail.com>
2025-10-11 05:08:19 +0530

    chore: add turbopack configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
+  },
+  turbopack: {
+    root: path.join(__dirname),
   },
 };
 
