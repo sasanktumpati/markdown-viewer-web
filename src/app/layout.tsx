8de05c54f7cd1623168d96d62ea3768bd35aaefe
sasanktumpati <sasanktumpati16@gmail.com>
2025-10-10 23:12:07 +0530

    chore: adds favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,24 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://md.built.systems",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
+      },
+    ],
+  },
   openGraph: {
     title: "Live Markdown Editor & Viewer with Mermaid Diagrams",
     description:
